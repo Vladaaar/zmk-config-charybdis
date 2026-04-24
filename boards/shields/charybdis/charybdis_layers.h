1b2b19ad361954c43620c212c134dc7b1893a91f
Vladaaar <48259012+Vladaaar@users.noreply.github.com>
2026-04-24 23:51:52 +0300

    my layers

--- boards/shields/charybdis/charybdis_layers.h
+++ boards/shields/charybdis/charybdis_layers.h
@@ -1,20 +1,17 @@
 /*
  * Copyright (c) 2024 The ZMK Contributors
  * SPDX-License-Identifier: MIT
- * 
+ *
  * Common layer definitions for Charybdis keyboard
  * Shared across all shield variants
  */
-
 #pragma once
 
 // Layer definitions matching the keymap
-#define BASE 0
-#define POINTER 1
-#define LOWER 2
-#define RAISE 3
-#define SYMBOLS 4
-#define SCROLL 5
-#define SNIPING 6
-#define GAMING 7
-
+#define BASE     0
+#define SYMBOLS  1
+#define NAV_EDIT 2
+#define FUNCTIONS 3
+#define SCROLL   4
+#define SNIPE    5
+#define GAME     6
